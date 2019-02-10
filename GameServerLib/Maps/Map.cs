6ca89742e0e90055e3a4877157fb1337b7f05589
Christophe Huybrechts <huybchris@hotmail.com>
2019-02-10 22:34:20 +0100

    Initial addition of HowlingAbyss
    All the coordinates are temporary.
    Minions are invisible.
    Enviroment models like shopkeepers, poro's need to be added.

--- GameServerLib/Maps/Map.cs
+++ GameServerLib/Maps/Map.cs
@@ -57,7 +57,7 @@ namespace LeagueSandbox.GameServer.Maps
             var dict = new Dictionary<int, Type>
             {
                 // [0] = typeof(FlatTestMap),
-                [1] = typeof(SummonersRift)
+                [1] = typeof(SummonersRift),
                 // [2] = typeof(HarrowingRift),
                 // [3] = typeof(ProvingGrounds),
                 // [4] = typeof(TwistedTreeline),
@@ -65,7 +65,7 @@ namespace LeagueSandbox.GameServer.Maps
                 // [8] = typeof(CrystalScar),
                 // [10] = typeof(NewTwistedTreeline),
                 // [11] = typeof(NewSummonersRift),
-                // [12] = typeof(HowlingAbyss),
+                [12] = typeof(HowlingAbyss)
                 // [14] = typeof(ButchersBridge)
             };
 
